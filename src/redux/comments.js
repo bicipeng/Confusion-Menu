734dd0376ee806a3ec3f72f155834f923c17ef0b
bicipeng <bicipeng@gmail.com>
2019-11-29 21:28:41 -0500

    added

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,11 +2,17 @@ import { COMMENTS } from "../shared/comments";
 import * as ActionTypes from "./ActionTypes";
 export const Comments = (state = COMMENTS, action) => {
   switch (action.type) {
-    case ActionTypes.ADD_COMMENT:
+    case ActionTypes.ADD_COMMENT: 
+    console.log("******just see what is action.payload", action.payload)
       let newComment = action.payload;
+     
       newComment.id = state.length;
       newComment.date=new Date().toISOString();
-      return [...state,newComment];
+      console.log("previous state",state)
+      
+      console.log("***&&what is the state", [...state,newComment])
+
+      return state.concat(newComment)
     default:
       return state;
   }
